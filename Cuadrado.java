a13e8af9e283923c0ed3ee63e7fd8ba7f3a9dc22
negrit01034 <163154181+negrit01034@users.noreply.github.com>
2024-04-02 09:23:03 -0500

    Add files via upload

---
+++ Cuadrado.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_objeto_cuadrado;
+
+/**
+ *
+ * @author Equipo Yefry
+ */
+public class Cuadrado {
+    
+    
+   public int lado;
+    public int calculararea(int lado){
+        int area;
+        area=lado*lado;
+        return area;
+    }
+    
+    public int calcularperimetro(int lado){
+        int perimetro;
+        perimetro=lado+lado+lado+lado;
+        return perimetro;        
+    }
+    
+    
+    
+}   
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
---
+++ Proyecto_Objeto_Cuadrado.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_objeto_cuadrado;
+
+/**
+ *
+ * @author Equipo Yefry
+ */
+public class Proyecto_Objeto_Cuadrado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        int lado=5;
+        Cuadrado Cuadrado1=new Cuadrado();
+        System.out.println(Cuadrado1.calculararea(lado));
+        
+        
+        Cuadrado Cuadrado2=new Cuadrado();
+        System.out.println(Cuadrado2.calcularperimetro(lado));
+        
+                
+        
+        
+        
+        
+        
+    }
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
